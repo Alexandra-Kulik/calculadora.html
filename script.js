9a6ff7b1625333f67760a9428aeee8f1667b5406
Alexandra-Kulik <168195696+Alexandra-Kulik@users.noreply.github.com>
2025-04-03 19:37:04 -0300

    01 tentativa

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+function appendValue(value) {
+    document.getElementById('screen').value += value;
+}
+function clearScreen() {
+    document.getElementById('screen').value = '';
+}
+function calculate() {
+    try {
+        document.getElementById('screen').value = eval(document.getElementById('screen').value);
+    } catch (e) {
+        alert('Erro na expressão!');
+    }
+}
